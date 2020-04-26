ca2f04ebbe0eb3cb87b7dc175415ad291d0e88bb
Willyan Santos <willyan41@hotmail.com>
2020-04-26 08:59:47 -0300

    Tratando erros personalizados

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -8,19 +8,25 @@ namespace JogoXadrezConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
-
-
-
-
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 2));
 
 
-            Tela.imprimirTabuleiro(tab);
 
+
+
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
     }
