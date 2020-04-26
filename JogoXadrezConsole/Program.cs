dc7e4e21d91f4a10c39e4dbfd2b01970750521a5
Willyan Santos <willyan41@hotmail.com>
2020-04-26 09:30:50 -0300

    Imprimir tabuleiro com melhorias

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -8,13 +8,25 @@ namespace JogoXadrezConsole
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 2));
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
 
 
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(3, 5));
+
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
     }
